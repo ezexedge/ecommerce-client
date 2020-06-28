a0df6a9b443b93a97738efe04e570fb1f47b9971
ezequiel <ezeedge@gmail.com>
2020-06-27 22:22:50 -0300

    agregando signup

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React , {useState} from 'react';
 import Layout from '../core/Layout'
 import { API } from '../config'
 const Signup = () => {
+
+    const [values,setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+
+    const handleChange = name => event => (
+            setValues({...values,error:false,[name]:event.target.value})
+    )
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} type="text" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} type="email" className="form-control"/>
+            </div>
+              
+               <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control"/>
+            </div>
+            <buttom className="btn btn-primary">submit</buttom>
+        </form>
+    )
+
     return ( 
-        <Layout title="Signup" description="Signup node react ecommerce">
-        {API}
+        <Layout title="Signup" description="Signup node react ecommerce"
+        className="container col-md-8 offset-md-2"
+        >
+        {signUpForm()}
+        {JSON.stringify(values)}
     </Layout>
      );
 }
