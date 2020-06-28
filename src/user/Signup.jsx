3120038d006e6faf323a25de3950eee9f01351da
ezequiel <ezeedge@gmail.com>
2020-06-27 23:37:59 -0300

    agregando mensajes de error o exito al logearse

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,4 +1,5 @@
 import React , {useState} from 'react';
+import {Link} from 'react-router-dom'
 import Layout from '../core/Layout'
 import { API } from '../config'
 const Signup = () => {
@@ -11,37 +12,93 @@ const Signup = () => {
         success: false
     })
 
+    const {name,email,password,error,success} = values
 
     const handleChange = name => event => (
             setValues({...values,error:false,[name]:event.target.value})
     )
 
+    const signup = (user) => {
+          //  console.log(name,email,password)
+
+      return fetch(`${API}/signup`,{
+              method: "POST",
+              headers: {
+                  Accept: 'application/json',
+                  "Content-type": "application/json"
+              },
+              body: JSON.stringify(user)
+          })
+          .then(response => {
+              return response.json()
+          })
+          .catch(err => {
+              console.log(err)
+          })
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault()
+        setValues({...values,error:false})
+        signup({name,email,password})
+        .then(data => {
+            if(data.error){
+                setValues({...values,error:data.error, success:false})
+            }else{
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    success: true
+                })
+            }
+        })
+        
+    }
+
     const signUpForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control"/>
+                <input onChange={handleChange('name')} value={name} type="text" className="form-control"/>
+
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control"/>
+                <input onChange={handleChange('email')} value={email} type="email" className="form-control"/>
             </div>
               
                <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control"/>
+                <input onChange={handleChange('password')} value={password} type="password" className="form-control"/>
             </div>
-            <buttom className="btn btn-primary">submit</buttom>
+            <button onClick={clickSubmit} className="btn btn-primary">submit</button>
         </form>
     )
+
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
+
+      const showSuccess = () => (
+        <div className="alert alert-info" style={{display: success ? '' : 'none'}}>
+            nueva cuenta creada , porfavor <Link to="/signin" > Ingresa </Link>
+        </div>
+      )
 
     return ( 
         <Layout title="Signup" description="Signup node react ecommerce"
         className="container col-md-8 offset-md-2"
         >
+         {showError()}
+         {showSuccess()}   
         {signUpForm()}
-        {JSON.stringify(values)}
     </Layout>
      );
 }
